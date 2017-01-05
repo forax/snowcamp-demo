bc961cf52399e53353fa8fda4f442a8c7b21a8df
Alexis Hassler <alexis.hassler@sewatech.fr>
2017-01-05 10:29:18 +0100

    Load XML configuration file with JAX-B

---
+++ legacy1/src/main/java/io/snowcamp/vertx/Configuration.java
@@ -0,0 +1,23 @@
+package io.snowcamp.vertx;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Configuration {
+    private int port;
+
+    public int getPort() {
+        return port;
+    }
+
+    @XmlElement
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    @Override
+    public String toString() {
+        return "Configuration [port=" + port + "]" ;
+    }
+}
