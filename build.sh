caaccd91194a58586f8f4b33fdcd30d5661c0365
Alexis Hassler <alexis.hassler@sewatech.fr>
2017-01-07 20:16:59 +0100

    Fix build with merged applications

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ export javac=$JAVA8_HOME/bin/javac
 
 mkdir -p target && rm -rf target/*
 cp -r src/main/resources/* target/
-$javac -classpath deps/vertx-core-3.3.3.jar:deps/vertx-web-3.3.3.jar \
+$javac -classpath deps/json-20160810.jar:deps/vertx-core-3.3.3.jar:deps/vertx-web-3.3.3.jar \
        -d target/ \
        $(find src/main/java/ -name "*.java")
 $javac -classpath target:deps/json-20160810.jar:deps/junit-4.12.jar \
@@ -14,7 +14,10 @@ $javac -classpath target:deps/json-20160810.jar:deps/junit-4.12.jar \
        $(find src/test/java/ -name "*.java")
 
 $java -classpath target:deps/vertx-core-3.3.3.jar:deps/vertx-web-3.3.3.jar:deps/vertx-internal-3.3.3.jar \
+      --add-modules java.xml.bind --add-opens java.base/java.nio=ALL-UNNAMED --add-opens java.base/sun.nio.ch=ALL-UNNAMED \
+      --add-exports java.base/sun.net.dns=ALL-UNNAMED -Dsun.reflect.debugModuleAccessChecks=true \
       io.snowcamp.papaya.web.ExampleApp
 
 #$java -classpath target:deps/json-20160810.jar:deps/junit-4.12.jar:deps/hamcrest-core-1.3.jar \
 #      org.junit.runner.JUnitCore io.snowcamp.papaya.test.TestSuite
+
