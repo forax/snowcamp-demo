f0285e2bf4d4d54c85483f65afabd4e313cf436a
Alexis Hassler <alexis.hassler@sewatech.fr>
2017-01-07 19:25:58 +0100

    Merge vertx jar files

--- build.sh
+++ build.sh
@@ -13,10 +13,8 @@ $javac -classpath target:deps/json-20160810.jar:deps/junit-4.12.jar \
        -d target/ \
        $(find src/test/java/ -name "*.java")
 
-$java -classpath target:deps/vertx-core-3.3.3.jar:deps/vertx-web-3.3.3.jar:deps/netty-transport-4.1.5.Final.jar:deps/netty-common-4.1.5.Final.jar:deps/netty-resolver-4.1.5.Final.jar:deps/netty-resolver-dns-4.1.5.Final.jar:deps/netty-codec-http-4.1.5.Final.jar:deps/netty-codec-4.1.5.Final.jar:deps/netty-buffer-4.1.5.Final.jar:deps/netty-handler-4.1.5.Final.jar:deps/netty-codec-dns-4.1.5.Final.jar:deps/netty-codec-http2-4.1.5.Final.jar:deps/jackson-databind-2.7.4.jar:deps/jackson-core-2.7.4.jar:deps/jackson-annotations-2.7.0.jar \
-      --add-modules java.xml.bind   -Dsun.reflect.debugModuleAccessChecks=true \
+$java -classpath target:deps/vertx-core-3.3.3.jar:deps/vertx-web-3.3.3.jar:deps/vertx-internal-3.3.3.jar \
       io.snowcamp.papaya.web.ExampleApp
 
 #$java -classpath target:deps/json-20160810.jar:deps/junit-4.12.jar:deps/hamcrest-core-1.3.jar \
 #      org.junit.runner.JUnitCore io.snowcamp.papaya.test.TestSuite
-
