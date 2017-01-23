13487a7ff64acd321a0702c0218e6d23267dae40
Alexis Hassler <alexis.hassler@sewatech.fr>
2017-01-23 10:15:26 +0100

    Migrate 1st part notes to asciidoc

--- build.sh
+++ build.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
-export JAVA8_HOME=/usr/jdk/jdk1.8.0_65
+export JAVA8_HOME=/usr/jdk/jdk-8
 export JAVA9_HOME=/usr/jdk/jdk-9
 export java=$JAVA9_HOME/bin/java
 export javac=$JAVA8_HOME/bin/javac
 
+# Compile
+#   change this in part 2
 mkdir -p target && rm -rf target/*
 cp -r src/main/resources/* target/
 $javac -classpath deps/json-20160810.jar:deps/vertx-core-3.3.3.jar:deps/vertx-web-3.3.3.jar \
+       -XDignore.symbol.file \
        -d target/ \
        $(find src/main/java/ -name "*.java")
 $javac -classpath target:deps/json-20160810.jar:deps/junit-4.12.jar \
        -d target/ \
        $(find src/test/java/ -name "*.java")
 
+# Run
+#   change this in part 1
 $java -classpath target:deps/vertx-core-3.3.3.jar:deps/vertx-web-3.3.3.jar:deps/vertx-internal-3.3.3.jar \
-      --add-modules java.xml.bind --add-opens java.base/java.nio=ALL-UNNAMED --add-opens java.base/sun.nio.ch=ALL-UNNAMED \
-      --add-exports java.base/sun.net.dns=ALL-UNNAMED -Dsun.reflect.debugModuleAccessChecks=true \
       io.snowcamp.papaya.web.ExampleApp
 
 #$java -classpath target:deps/json-20160810.jar:deps/junit-4.12.jar:deps/hamcrest-core-1.3.jar \
